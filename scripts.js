bbae2c4900cb9e6dc8ce72cc8dfc9450b608ea1b
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-02-18 12:16:28 +0200

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function toggleForm() {
+  let form = document.getElementById("log-in");
+  form.style.display = form.style.display === "block" ? "none" : "block";
+}
+// document.getElementById('log-in').style.display='none'
+// document.getElementById('log-in').style.display='block'
