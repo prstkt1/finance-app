3d3ee99bd5e9aff92648085f1f203e568a1552cd
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-05-06 21:46:46 +0300

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("openLoginBtn").addEventListener("click", toggleForm);
+  document.getElementById("closeLogin").addEventListener("click", showAlert);
+});
+export const toggleForm = () => {
+  let loginForm = document.getElementById("log-in");
+  let loginButton = document.getElementById("login-button");
+  let logoutButton = document.getElementById("logout-button");
+
+  loginForm.style.display =
+    loginForm.style.display === "block" ? "none" : "block";
+  loginButton.style.display =
+    loginButton.style.display === "none" ? "block" : "none";
+  logoutButton.style.display =
+    logoutButton.style.display === "none" ? "block" : "none";
+};
+export const showAlert = (message) => {
+  const alert = document.getElementById("better_alert");
+  alert.textContent = message;
+  alert.classList.remove("hidden");
+  alert.classList.add("show");
+
+  setTimeout(() => {
+    alert.classList.remove("show");
+    alert.classList.add("hidden");
+  }, 3000);
+};
