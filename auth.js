c065000fd51029cd43723b7f5534c75cd76cc7bd
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-05-06 21:46:42 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,28 @@
+import { showExpenses } from "./scripts.js";
+import { showAlert, toggleForm } from "./ui.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("registerBtn").addEventListener("click", register);
+});
+const register = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("pass").value;
+
+  let users = JSON.parse(localStorage.getItem("users")) || {};
+
+  if (users[email]) {
+    showAlert("User already exists!");
+  } else {
+    users[email] = {
+      password: password,
+      expenses: [],
+    };
+    localStorage.setItem("users", JSON.stringify(users));
+    showAlert("Registration successful!");
+    localStorage.setItem("currentUser", email);
+    toggleForm();
+    showExpenses();
+  }
+};
+
+export { register };
