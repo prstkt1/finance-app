a9f020688ef65e1de4bc51e4329e6bce6a1af93e
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-05-06 23:49:28 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -25,4 +25,19 @@ const register = () => {
   }
 };
 
-export { register };
+export { register }; // Login
+export const login = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("pass").value;
+
+  let users = JSON.parse(localStorage.getItem("users")) || {};
+
+  if (users[email] && users[email].password === password) {
+    showAlert("Login successful!");
+    localStorage.setItem("currentUser", email);
+    toggleForm();
+    showExpenses();
+  } else {
+    showAlert("Invalid email or password!");
+  }
+};
