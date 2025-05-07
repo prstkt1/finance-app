df3177d8eeeeaa3e92f411a12cc4108a792850c6
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-05-07 12:14:13 +0300

    Create expenses.js

---
+++ expenses.js
@@ -0,0 +1,54 @@
+import { filterExpensesByMonth, updateChart } from "./scripts.js";
+import {
+  clearForm,
+  closeAddExpenseForm,
+  customSelect,
+  showAlert,
+  totalExpense,
+  updateMonthDisplay,
+} from "./ui.js";
+
+export let currentMonth = new Date().getMonth();
+export let currentYear = new Date().getFullYear();
+
+export const showExpenses = () => {
+  updateMonthDisplay();
+};
+// Add expense
+export const addExpense = () => {
+  const currentUser = localStorage.getItem("currentUser");
+  if (!currentUser) {
+    showAlert("Please log in first");
+    return;
+  }
+
+  let users = JSON.parse(localStorage.getItem("users")) || {};
+  let expenses = users[currentUser].expenses;
+
+  const name = document.getElementById("customInput").value;
+  const amount = Math.abs(document.getElementById("amount").value);
+  if (amount === 0) {
+    showAlert("Amount cannot be zero!");
+    return;
+  }
+  const date = new Date(currentYear, currentMonth, new Date().getDate())
+    .toISOString()
+    .split("T")[0];
+
+  let existingExpense = expenses.find(
+    (expense) => expense.name === name && expense.date === date
+  );
+  if (existingExpense) {
+    existingExpense.amount = amount + parseInt(existingExpense.amount);
+  } else {
+    expenses.push({ name: name, amount: amount, date: date });
+  }
+  localStorage.setItem("users", JSON.stringify(users));
+  filterExpensesByMonth(currentMonth, currentYear);
+  totalExpense();
+
+  customSelect(name);
+  clearForm();
+  closeAddExpenseForm();
+  updateChart();
+};
