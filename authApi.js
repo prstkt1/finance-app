ac0ee22a4c7884ca15794cc04f1809a9248b099b
prstkt1 <oliinyk.nikita@lll.kpi.ua>
2025-05-22 19:04:10 +0300

    Update authApi.js

--- authApi.js
+++ authApi.js
@@ -1,14 +1,15 @@
-export async function registerUser(email, password) {
+export const registerUser = async (email, password) => {
   return fetch("http://localhost:3000/users/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
   });
-}
-export async function loginUser(email, password) {
+};
+
+export const loginUser = async (email, password) => {
   return fetch("http://localhost:3000/users/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
   });
-}
+};
